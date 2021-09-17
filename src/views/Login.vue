feefb7e41da611adb753bf5f0fa1d4930a76f516
dweyne <893734359@qq.com>
2021-09-17 23:50:00 +0800

    登陆页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,35 @@
 <template>
-  <h1>login</h1>
+  <div class="login">
+    <!-- 登录框 -->
+    <div class="box">
+      <div class="login-box">
+        <div class="title">登录</div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.login{
+  width: 100%;
+  height: 100%;
+  background-image: url("../assets/login.jpg");
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-box{
+    width: 500px;
+    height: 500px;
+    background: rgba(255, 255, 255, .5);
+    border-radius: 10px;
+    .title{
+      color: #333;
+      font-size: 28px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
